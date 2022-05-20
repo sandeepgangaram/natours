8c5a0688afad679414d5814945f00d3310854591
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-05-20 19:00:55 +0530

    Added alert after bookingn

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -4,6 +4,7 @@ export const hideAlert = () => {
 };
 
 export const showAlert = (type, msg, time = 5) => {
+  hideAlert();
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
   window.setTimeout(hideAlert, time * 1000);
