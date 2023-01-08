a36f9bdfe54196d423e475ffe9d2c9ece8d401d9
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-01-08 15:01:19 +0530

    refactor routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -19,6 +19,7 @@ router.get('/checkout-session/:tourId', getCheckoutSession);
 router.use(restrictTo('admin', 'lead-guide'));
 
 router.route('/').get(getAllBookings).post(createBooking);
+
 router.route('/:id').get(getBooking).patch(updateBooking).delete(deleteBooking);
 
 module.exports = router;
